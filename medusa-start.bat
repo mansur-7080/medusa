29f315326de45f728da9071e47737b9ef56403fd
Sharipov Mansurbek <mansur.developer@gmail.com>
2025-06-21 17:36:14 +0500

    Bat fayllarni to'g'irlash va dependencies ni yangilash

--- medusa-start.bat
+++ medusa-start.bat
@@ -11,6 +11,10 @@ cd /d "d:\meduza\my-medusa-store"
 start "Medusa Backend" cmd /k "npm run dev"
 
 echo.
+echo Waiting 10 seconds for backend to start...
+timeout /t 10 /nobreak >nul
+
+echo.
 echo [2/2] Starting Medusa Storefront...
 echo Directory: d:\meduza\my-medusa-store-storefront
 cd /d "d:\meduza\my-medusa-store-storefront"
@@ -21,7 +25,7 @@ echo =======================================
 echo Services Starting...
 echo =======================================
 echo Backend will be available at: http://localhost:9000
-echo Storefront will be available at: http://localhost:3000
+echo Storefront will be available at: http://localhost:8000
 echo Admin Panel will be available at: http://localhost:9000/app
 echo.
 echo Close this window after services have started.
